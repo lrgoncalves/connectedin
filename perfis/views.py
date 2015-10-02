e1b9fac3a34f2caa58fede99d2d193f73f31e705
Leandro Gonçalves <leandro.goncalves@hotelurbano.com.br>
2015-10-02 14:23:36 -0300

    exercicios aula 3

--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+
+    perfil = Perfil();
+
+    if perfil_id == '1':
+        perfil = Perfil('Leandro', 'leandro@leandro.com', '878787799', 'HU')
+
+    return render(request, 'perfil.html', { "perfil" : perfil })
