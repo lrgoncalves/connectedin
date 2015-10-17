cf3cc65804e99c0ccbe88ee2a292ebb213ddbcc7
Leandro Gonçalves <leandro.goncalves@hotelurbano.com.br>
2015-10-17 15:29:37 -0300

    classe user

--- perfis/models.py
+++ perfis/models.py
@@ -1,16 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Perfil(models.Model):
 
     nome = models.CharField(max_length=255, null=False)
-    email = models.CharField(max_length=255, null=False)
     telefone = models.CharField(max_length=15, null=False)
     nome_empresa = models.CharField(max_length=255, null=False)
 
+    contatos = models.ManyToManyField('self')
+
+    usuario = models.OneToOneField(User, related_name="perfil")
+
+    @property
+    def email(self):
+        return self.usuario.email
+
     def convidar(self, perfil_convidado):
-        solicitante = Convite(solicitante=self, convidado=perfil_convidado).save()
+        Convite(solicitante=self, convidado=perfil_convidado).save()
 
 class Convite(models.Model):
     solicitante = models.ForeignKey(Perfil, related_name='convites_feitos')
     convidado = models.ForeignKey(Perfil, related_name='convites_recebidos')
+
+    def aceitar(self):
+        self.convidado.contatos.add(self.solicitante)
+        self.solicitante.contatos.add(self.convidado)
+        self.delete()
--- perfis/views.py
+++ perfis/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from perfis.models import Perfil
+from perfis.models import Perfil, Convite
 
 # Create your views here.
 def index(request):
@@ -8,12 +8,19 @@ def index(request):
 
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id = perfil_id);
-    return render(request, 'perfil.html', { "perfil" : perfil })
+    perfil_logado = get_perfil_logado(request)
+    ja_eh_contato = perfil in perfil_logado.contatos.all()
+    return render(request, 'perfil.html', { "perfil" : perfil, "ja_eh_contato" : ja_eh_contato })
 
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id = perfil_id)
     perfil_logado = get_perfil_logado(request)
     perfil_logado.convidar(perfil_a_convidar)
+    return redirect('index')
+
+def aceitar(request, convite_id):
+    convite = Convite.objects.get(id=convite_id)
+    convite.aceitar()
     return redirect('index')
 
 def get_perfil_logado(request):
