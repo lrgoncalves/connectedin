dd3a27d5f36858f2f96f2afe665505f20292e10a
Leandro Gonçalves <leandro.goncalves@hotelurbano.com.br>
2015-10-18 14:01:57 -0200

    finalizando connectedin

--- connectedin/settings.py
+++ connectedin/settings.py
@@ -58,10 +58,21 @@ WSGI_APPLICATION = 'connectedin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'connectedin',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -83,3 +94,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL="/login/"
+LOGOUT_URL="/logout/"
+LOGIN_REDIRECT_URL="/"
